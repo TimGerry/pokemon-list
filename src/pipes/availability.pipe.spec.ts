03bb5245de9f50aeb3398e9565fd43f54159c146
TimGe <timgerry.geerts@infosupport.com>
2024-02-12 11:23:44 +0100

    Add pipe test

--- src/pipes/availability.pipe.spec.ts
+++ src/pipes/availability.pipe.spec.ts
@@ -1,8 +1,35 @@
 import { AvailabilityPipe } from './availability.pipe';
 
 describe('AvailabilityPipe', () => {
+  let sut: AvailabilityPipe;
+
+  beforeEach(() => {
+    sut = new AvailabilityPipe();
+  })
+
   it('create an instance', () => {
-    const pipe = new AvailabilityPipe();
-    expect(pipe).toBeTruthy();
+    expect(sut).toBeTruthy();
+  });
+
+  it('should be available', () => {
+    // Arrange
+    const myPipeInput = 'some input'
+    
+    // Act
+    const actual = sut.transform(myPipeInput);
+
+    // Assert
+    expect(actual).toBe(myPipeInput);
+  });
+
+  it('should not be available', () => {
+    // Arrange
+    const myPipeInput = undefined
+    
+    // Act
+    const actual = sut.transform(myPipeInput);
+
+    // Assert
+    expect(actual).toBe('Unavailable');
   });
 });
